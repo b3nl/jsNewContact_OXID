32413342d6e38259e1eae074277c4559a3b8a89d
Juliana <juliana.schuster@pixolith.de>
2015-08-20 14:09:51 +0200

    +database

--- copy_this/modules/jsNewContact_OXID/controller/jsnewcontacthandledatabase.php
+++ copy_this/modules/jsNewContact_OXID/controller/jsnewcontacthandledatabase.php
@@ -14,7 +14,8 @@
             $oDb = oxDb::getDb();
 
             $create_sql = "INSERT INTO `oxcontents` (`OXID`, `OXLOADID`, `OXSHOPID`, `OXSNIPPET`, `OXTYPE`, `OXACTIVE`, `OXACTIVE_1`, `OXPOSITION`, `OXTITLE`, `OXCONTENT`, `OXTITLE_1`, `OXCONTENT_1`, `OXACTIVE_2`, `OXTITLE_2`, `OXCONTENT_2`, `OXACTIVE_3`, `OXTITLE_3`, `OXCONTENT_3`, `OXCATID`, `OXFOLDER`, `OXTERMVERSION`) VALUES
-                        ('5138685f66ec2613ff84b8e0b073c712', 'openingHours', 'oxbaseshop', 1, 0, 1, 0, '', 'Öffnungszeiten im Kontaktformular', '<b>Ihre Öffnungszeiten<b>', '', '', 0, '', '', 0, '', '', '943a9ba3050e78b443c16e043ae60ef3', '', '')";
+                            ('5138685f66ec2613ff84b8e0b073c712', 'openingHours', 'oxbaseshop', 1, 0, 1, 0, '', 'Öffnungszeiten im Kontaktformular', '<b>Ihre Öffnungszeiten:<b></br></br>Mo. - Do.: 10.00 - 20.00 Uhr</br>Fr.: 10.00 - 22.00 Uhr</br>
+                            Sa.: 10.00 - 20.00 Uhr', '', '', 0, '', '', 0, '', '', '943a9ba3050e78b443c16e043ae60ef3', '', '')";
 
             $oDb->execute($create_sql);
             return true;
